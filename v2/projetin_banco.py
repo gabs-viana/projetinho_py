b0ab3a8ff1b39822dae553da4529d720639732b4
Gabs.dViana <bielmviana@gmail.com>
2024-04-07 20:40:08 -0300

    Terminando projeto Banco v2

--- v2/projetin_banco.py
+++ v2/projetin_banco.py
@@ -107,3 +107,59 @@ class ContaCorrente(Conta):
             C/C: \t\t{self.numero}
             Titular: \t{self.cliente.nome}
 """
+    
+class Historico:
+    def __init__(self):
+        self._transacoes = []
+
+    @property
+    def transacoes(self):
+        return self._transacoes
+    
+    def adicionar_transacao(self, transacao):
+        self._transacoes.append(
+            {
+                "tipo": transacao.__class__.__name__,
+                "valor": transacao.valor,
+                "data": datetime.now().strftime
+                ("%d-%m-%Y %H:%M:%s"),
+            }
+        )
+
+class Transacao(ABC):
+    @property
+    @abstractproperty
+    def valor(self):
+        pass
+
+    @abstractclassmethod
+    def registrar(self, conta):
+        pass
+
+class Saque(Transacao):
+    def __init__(self, valor):
+        self._valor = valor
+
+    @property
+    def valor(self):
+        return self._valor
+    
+    def registrar(self, conta):
+        sucesso_transacao = conta.sacar(self.valor)
+
+        if sucesso_transacao:
+            conta.historico.adicionar_transacao(self)
+
+class Deposito(Transacao):
+        def __init__(self, valor):
+            self._valor = valor
+
+        @property
+        def valor(self):
+            return self._valor
+    
+        def registrar(self, conta):
+            sucesso_transacao = conta.depositar(self.valor)
+
+            if sucesso_transacao:
+                conta.historico.adicionar_transacao(self)
